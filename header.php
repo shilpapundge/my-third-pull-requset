b608ef43462c78fe0374000516f1af4cbaa62c31
shilpapundge <shilpapundge1997@gmail.com>
2019-10-26 21:47:28 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,46 @@
+
+<html lang="en">
+<head>
+  <title>User </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="public/css/main.css">
+  <link rel="stylesheet" href="../public/css/main.css">
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#"></a></li>
+      <li><a href="locatedustbin.php"> Locate Bin</a></li>
+      <li><a href="#"> Add Bin</a></li>
+      <li><a href="#"> Collect my Waste</a></li>
+      <li><a href="#"> Disposable Techniques</a></li>
+      <li><a href="#"> Upload a complaint</a></li>
+      <li><a href="notifications.php"> Notifications</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-user"></span> User Profile<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+<li>
+      <a href="https://github.com/shilpapundge"> 
+        ShilpaPundge
+</li>
+          <li><a href="authentication/login.php">Logout</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</nav>
+  
+
+    
+
+
+</body>
+</html>
